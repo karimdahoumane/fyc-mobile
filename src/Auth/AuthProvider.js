a4105531033bc2b6c829b2038011b827bda303f3
Your Karim DAHOUMANE <krmdahoumane@gmail.com>
2023-02-06 00:17:11 +0100

    (Fix): Home and Profile

--- src/Auth/AuthProvider.js
+++ src/Auth/AuthProvider.js
@@ -1,18 +1,9 @@
-import { API_URL } from "../Utils/Constants";
 import { getToken } from "../Auth/TokenProvider";
+import jwt_decode from 'jwt-decode';
 
 export const getCurrentUser = async () => {
-    try {
-        const response = fetch(API_URL + "users/13", {
-            method: "GET",
-            headers: {
-                "Content-Type": "application/json",
-                "Authorization": "Bearer " + await getToken(),
-            },
-        });
-        const json = await response.json();
-        return json;
-    } catch (error) {
-        console.error(error);
-    }
-};
+    const token = await getToken();
+    const user = await jwt_decode(token);
+    console.log(user);
+    return user;
+}
--- src/Screens/Profile.js
+++ src/Screens/Profile.js
@@ -1,11 +1,11 @@
 import React from "react";
 import { SafeAreaView, StyleSheet, Text } from "react-native";
 import { TextInput } from "react-native-gesture-handler";
-import { Button } from "react-native-elements";
 import { View } from "react-native";
 import { useEffect } from "react";
 import { API_URL } from "../Utils/Constants";
 import { getToken } from "../Auth/TokenProvider";
+import { getCurrentUser } from "../Auth/AuthProvider";
 
 
 
@@ -15,9 +15,11 @@ const Profile = () => {
   const [error, setError] = React.useState("");
 
   useEffect(() => {
+    
     const fetchData = async () => {
       try {
-        const response = await fetch(API_URL + "users/13", {
+        const user = await getCurrentUser()
+        const response = await fetch(API_URL + "users/" + user.sub, {
           method: "GET",
           headers: {
             "Content-Type": "application/json",
@@ -25,6 +27,7 @@ const Profile = () => {
           },
         });
         const json = await response.json();
+        console.log(json)
         setEmail(json.email);
         setNickname(json.nickname);
       } catch (error) {
@@ -34,46 +37,23 @@ const Profile = () => {
     fetchData();
   }, []);
 
-  const handleUpdate = async () => {
-    try {
-      const response = await fetch(API_URL + "users/me", {
-        method: "PUT",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: "Bearer " + (await getToken()),
-        },
-        body: JSON.stringify({
-          nickname,
-        }),
-      });
-      const json = await response.json();
-      if (json.nickname) {
-        setError("");
-      } else {
-        setError("Something went wrong, please try again");
-      }
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
-
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.titleItem}>
         <Text style={styles.titleText}>Mon profil</Text>
       </View>
       <View style={styles.editItem}>
+      <Text style={styles.labelItem}>Email</Text>
       <TextInput style={styles.textInput}
         onChangeText={setEmail}
         value={email}
         disabled
       />
+      <Text style={styles.labelItem}>Pseudo</Text>
       <TextInput style={styles.textInput}
         onChangeText={setNickname}
         value={nickname}
       />
-      <Button style={styles.submitButton} title="Update" onPress={handleUpdate} />
       <Text style={{ color: "red" }}>{error}</Text>
       </View>
     </SafeAreaView>
@@ -83,33 +63,37 @@ const Profile = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#000000",
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
   titleItem: {
     flex: 1,
-    justifyContent: "center",
     alignItems: "center",
+    justifyContent: "center",
   },
   titleText: {
-    color: "#FFFFFF",
     fontSize: 30,
+    fontWeight: "bold",
   },
   editItem: {
     flex: 3,
-    justifyContent: "center",
     alignItems: "center",
+    justifyContent: "center",
+  },
+  labelItem: {
+    fontSize: 20,
+    fontWeight: "bold",
+    marginBottom: 10,
   },
   textInput: {
     height: 40,
     width: 300,
     borderColor: "gray",
     borderWidth: 1,
-    marginTop: 20,
-    color: "#FFFFFF",
+    marginBottom: 20,
   },
-  submitButton: {
-    marginTop: 20,
-  },
+  
   
 });
 
